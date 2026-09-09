36a38f4e0cfa99715360b1fdcb1af07c46321772
sabbumia <dwmaster22@gmail.com>
2026-09-09 22:55:17 +0600

    Add files via upload

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)','/','/doctors','/pricing','/health-tips','/contact'])
+const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)','/','/doctors','/pricing','/health-tips','/contact', '/emergency'])
 
 export default clerkMiddleware(async (auth, req) => {
   if (!isPublicRoute(req)) {
